d6546b91f0792107ce0778f3e4efbdd8867d57ca
yurii macbook pro <yurikorock777@gmail.com>
2025-09-27 23:49:32 +0300

    feat: add HomePage ShoppingCartPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./Layout/Layout.jsx";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage.jsx";
+import HomePage from "../pages/HomePage/HomePage.jsx";
+import ShoppingCartPage from "../pages/ShoppingCartPage/ShoppingCartPage.jsx";
 
 export default function App() {
   return (
     <div>
-      <h1>Best selling</h1>
+
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="shopping" element={<ShoppingCartPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
       </Routes>
     </div>
   );
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,18 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <div className="container">
-      Layout
+
       <header>
         <nav>
           <NavLink to="/">Shop</NavLink>
           <NavLink to="/shopping">Shopping Cart</NavLink>
         </nav>
       </header>
+      <main>
+        <Outlet/>
+      </main>
     </div>
   );
 }
